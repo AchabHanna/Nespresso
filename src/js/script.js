13b53ac13ebba8d03dba603d34e0e534b0f56e30
AchabHanna <hanna.achab@hetic.net>
2020-05-11 15:11:07 +0200

    added js

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,27 @@
+const vertuoRed = document.getElementById("vertuoRed");
+const vertuoRedColor = document.getElementById("vertuoRedColor");
+const vertuoBlackColor = document.getElementById("vertuoBlackColor");
 
+vertuoRedColor.addEventListener("click", () => {
+  vertuoRed.style.display = "block";
+  vertuoBlack.style.display = "none";
+});
 
+vertuoBlackColor.addEventListener("click", () => {
+  vertuoBlack.style.display = "block";
+  vertuoRed.style.display = "none";
+});
 
+const citizRed = document.getElementById("citizRed");
+const citizRedColor = document.getElementById("citizRedColor");
+const citizGreyColor = document.getElementById("citizGreyColor");
 
+citizGreyColor.addEventListener("click", () => {
+  citizGrey.style.display = "block";
+  citizRed.style.display = "none";
+});
+
+citizRedColor.addEventListener("click", () => {
+  citizRed.style.display = "block";
+  citizGrey.style.display = "none";
+});
